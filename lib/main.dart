9bdaa19b960f94f0e1dacd239e67a9882cba3f32
Monir Itani <monir_itani@hotmail.com>
2024-01-17 18:15:37 +0200

    Integrate Riverpod follow up
    - Added provider scope to the main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'app/app.dart';
 import 'package:flutter/material.dart';
 
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const App());
+  runApp( const ProviderScope(child: App()));
 }
