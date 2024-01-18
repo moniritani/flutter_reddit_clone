9238a58241b3df91a5e9e6858f63babee336b7fa
Monir Itani <monir_itani@hotmail.com>
2024-01-18 13:30:47 +0200

    Updated the user header widget
    - Updates to the custom header widget to adapt having time stamps and tags

--- lib/presentation/widgets/user_header_widget.dart
+++ lib/presentation/widgets/user_header_widget.dart
@@ -5,26 +5,58 @@ import 'package:reddit_clone/data/models/models.dart';
 class UserHeaderWidget extends StatelessWidget {
   final User user;
   final double avatarSize;
+  final MainAxisAlignment alignment;
+  final bool showTag;
+  final String? timestamp;
 
-  const UserHeaderWidget({super.key,
+  const UserHeaderWidget({
+    Key? key,
     required this.user,
-    this.avatarSize = 15.0
-  });
+    this.avatarSize = 15.0,
+    this.alignment = MainAxisAlignment.center,
+    this.timestamp,
+    this.showTag = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CircleAvatar(
-          radius: avatarSize,
-          backgroundImage: NetworkImage(user.avatarUrl),
+        Row(
+          mainAxisAlignment: alignment,
+          children: [
+            CircleAvatar(
+              radius: avatarSize,
+              backgroundImage: NetworkImage(user.avatarUrl),
+            ),
+            const Gap(Dimens.spacingXSmall),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      user.username,
+                      style: context.labelLargeStyle,
+                    ),
+                    if (timestamp != null) ...[
+                      const Gap(Dimens.spacingTiny),
+                      const Text("\u00B7", style: TextStyle(fontWeight: FontWeight.bold)),
+                      const Gap(Dimens.spacingTiny),
+                      Text(timestamp!, style: context.labelSmallStyle),
+                    ],
+                  ],
+                ),
+                if (showTag && user.tag != null) ...[
+                  Text(user.tag!, style: context.labelSmallStyle),
+                ],
+              ],
+            ),
+
+          ],
         ),
-        const Gap(Dimens.spacingXSmall),
-        Text(
-          user.username,
-          style: context.labelLargeStyle,
-        ),
+
       ],
     );
   }
