eac81a9392bfd998e6d4c7d816b7320752182e4d
Monir Itani <monir_itani@hotmail.com>
2024-01-18 10:00:51 +0200

    Added a custom image button

---
+++ lib/presentation/widgets/image_button_widget.dart
@@ -0,0 +1,45 @@
+import 'package:reddit_clone/app/app_dependencies.dart';
+
+class ImageButton extends StatelessWidget {
+  final Image icon;
+  final String? label;
+  final double verticalPadding;
+  final double horizontalPadding;
+  final TextStyle? textStyle;
+  final VoidCallback onPressed;
+
+  const ImageButton({
+    Key? key,
+    required this.icon,
+    this.label,
+    this.verticalPadding = Dimens.spacingSmall,
+    this.horizontalPadding = 0,
+    required this.onPressed,
+    this.textStyle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: verticalPadding ,horizontal: horizontalPadding),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          GestureDetector(
+            onTap: onPressed,
+            child:icon,
+          ),
+          if (label != null)
+            Padding(
+              padding: const EdgeInsets.only(top: Dimens.spacingNano),
+              child: Text(
+                label!,
+                style: textStyle ?? context.bodyMediumStyle,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
