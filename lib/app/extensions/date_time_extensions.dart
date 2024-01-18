88aa9971d842335733cc350db00c1b82b527f79c
Monir Itani <monir_itani@hotmail.com>
2024-01-18 13:33:51 +0200

    Added datetime extensions
    - Added a datetime extension to extract time tags

---
+++ lib/app/extensions/date_time_extensions.dart
@@ -0,0 +1,33 @@
+
+
+extension DateTimeExtensions on DateTime {
+
+
+  String get timeTag {
+    final now = DateTime.now();
+    final difference = now.difference(this);
+
+    if (difference.inDays > 365) {
+      return '${(difference.inDays / 365).floor()} years ago';
+    } else if (difference.inDays > 30) {
+      return '${(difference.inDays / 30).floor()} months ago';
+    } else if (difference.inDays > 7) {
+      return '${(difference.inDays / 7).floor()} weeks ago';
+    } else if (difference.inDays >= 2) {
+      return '${difference.inDays} days ago';
+    } else if (difference.inDays >= 1) {
+      return 'Yesterday';
+    } else if (difference.inHours >= 2) {
+      return '${difference.inHours} hours ago';
+    } else if (difference.inHours >= 1) {
+      return '1 hour ago';
+    } else if (difference.inMinutes >= 2) {
+      return '${difference.inMinutes} minutes ago';
+    } else if (difference.inMinutes >= 1) {
+      return '1 minute ago';
+    } else {
+      return 'Just now';
+    }
+  }
+
+}
